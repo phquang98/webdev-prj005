b5c8e05d494f2bad4f11938f22558f0179f24b5a
phquang98 <phquang98@gmail.com>
2021-04-21 14:32:15 +0300

    clean the repo and prepare envi for new exercise

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { FC, Fragment } from "react";
+
+var asd = 1;
+
+const App: FC = () => {
+  return (
+    <Fragment>
+      <div className="App">
+        <p>Hello</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>,
+  document.getElementById("root")
+);
